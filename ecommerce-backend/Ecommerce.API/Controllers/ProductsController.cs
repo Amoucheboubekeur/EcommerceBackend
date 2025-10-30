7abc6b5249506bd3b0a8431db36419703c2afded
Amoucheboubekeur <120303252+Amoucheboubekeur@users.noreply.github.com>
2025-10-30 11:12:26 +0100

    Update ProductsController.cs

--- ecommerce-backend/Ecommerce.API/Controllers/ProductsController.cs
+++ ecommerce-backend/Ecommerce.API/Controllers/ProductsController.cs
@@ -10,7 +10,7 @@ namespace Ecommerce.API.Controllers;
 [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 [ApiController]
 [Route("api/[controller]")]
-[Authorize(Roles = "SuperAdmin")] // Seul le SuperAdmin peut accéder ici
+[Authorize(Roles = "SuperAdmin,Admin")] // Seul le SuperAdmin peut accéder ici
 
 
     public class ProductsController(IProductService service) : ControllerBase
